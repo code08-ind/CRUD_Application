6bbfc0335e8963ab1012300aacd12cf052512fa5
Aryan Garg <69002338+code08-ind@users.noreply.github.com>
2023-03-02 19:43:51 +0530

    Added Documentation In Schema File

--- server/schema/user-schema.js
+++ server/schema/user-schema.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import autoIncrement from 'mongoose-auto-increment';
 
+// Added the schema for the Database with attributes defined below
 const userSchema = mongoose.Schema({
     name:String,
     username:String,
@@ -11,6 +12,7 @@ const userSchema = mongoose.Schema({
 autoIncrement.initialize(mongoose.connection);
 userSchema.plugin(autoIncrement.plugin, 'user');
 
+// Model is created out and it will be exported to be used in other files
 const user = mongoose.model('user', userSchema);
 
 export default user;
