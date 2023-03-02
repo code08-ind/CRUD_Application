cde5320ac9a7abb2236ab376fcf3be16dd7f29f2
Aryan Garg <69002338+code08-ind@users.noreply.github.com>
2023-03-02 22:56:42 +0530

    Updated The Documentaion

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import Routes from './routes/route.js';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
+// making it a express app
 const app = express();
 
 dotenv.config();
@@ -18,9 +19,12 @@ app.use('/', Routes);
 
 const PORT = 8000;
 
+// saving in the env file so that they remain safe and it provides security to important data and credentials
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
+// setting up connection
 Connection(username, password);
 
+// listening a port and making it run
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
