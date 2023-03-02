f095490cdb7d1ff40c5730c7e7e83abce0340c0c
Aryan Garg <69002338+code08-ind@users.noreply.github.com>
2023-03-02 23:08:13 +0530

    Updated The Documentation in index file

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// creating a root by including a div
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// creating and rendering a react app with App component being rendered
 root.render(
   <React.StrictMode>
     <App />
