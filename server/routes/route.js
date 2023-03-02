60373f5c9a5bdd247d9b3da04ba9b0ed0a7623a4
Aryan Garg <69002338+code08-ind@users.noreply.github.com>
2023-03-02 19:48:02 +0530

    Added Documentation For Routes

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,9 +1,12 @@
 import express from 'express';
 
+// Functions are imported from controllers which are used for handling requests
 import { editUser, addUser, getUsers, getUser, deleteUser } from '../controller/user-controller.js';
 
+// Router is imported from express
 const router = express.Router();
 
+// Routes are created with different facility such as get, post, put and delete requests
 router.post('/add', addUser);
 router.put('/:id', editUser);
 router.get('/all', getUsers);
